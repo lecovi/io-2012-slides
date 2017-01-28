415c4983a497470bab3454ee654d512eee8ad9e1
Leandro E. Colombo Viña <colomboleandro@bitson.com.ar>
2017-01-28 12:18:03 -0300

    Presentacion del Curso v1

--- slide_config.js
+++ slide_config.js
@@ -1,18 +1,18 @@
 var SLIDE_CONFIG = {
   // Slide settings
   settings: {
-    title: 'Title Goes Here<br>Up To Two Lines',
-    subtitle: 'Subtitle Goes Here',
-    //eventInfo: {
-    //  title: 'Google I/O',
-    //  date: '6/x/2013'
-    //},
+    title: 'Desarrollando aplicaciones Web<br>con Python',
+    subtitle: 'Utilizando Flask como framework',
+    eventInfo: {
+     title: 'Curso Udemy',
+     date: '28/02/2017'
+    },
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/google_developers_logo_tiny.png',
+    favIcon: 'images/Bitson-IsoNuevo_24x24.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
@@ -22,12 +22,12 @@ var SLIDE_CONFIG = {
 
   // Author information
   presenters: [{
-    name: 'Firstname Lastname',
-    company: 'Job Title<br>Google',
-    gplus: 'http://plus.google.com/1234567890',
-    twitter: '@yourhandle',
-    www: 'http://www.you.com',
-    github: 'http://github.com/you'
+    name: 'Prof. Leandro E. Colombo Viña',
+    company: 'DevOps Engineer<br>Cooperativa de Trabajo BITSON Ltda.',
+    // gplus: 'https://plus.google.com/+LeandroEColomboViña',
+    twitter: '@lecovi',
+    www: 'http://leo.bitson.com.ar',
+    github: 'http://github.com/lecovi'
   }/*, {
     name: 'Second Name',
     company: 'Job Title, Google',
@@ -37,4 +37,3 @@ var SLIDE_CONFIG = {
     github: 'http://github.com/you'
   }*/]
 };
-
